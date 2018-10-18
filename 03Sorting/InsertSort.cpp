c789163e44fa396cfe98fbec2350a427163670dc
chsyisgood <chsy973414741@gmail.com>
2018-10-17 20:14:18 -0400

    Update 03Sorting/InsertSort.cpp

---
+++ 03Sorting/InsertSort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void InsertSort(int x[], int n){
+    int temp = 0;
+    for(int i = 1; i < n; i++){
+        temp = x[i];
+        int j = i - 1;
+        for(j = i - 1; j >= 0; j--){
+            if(x[j] > temp){
+                x[j+1] = x[j];
+            }
+            else{
+                x[j+1] = temp;
+                break;
+            }
+        }
+        x[j+1] = temp;
+    }
+}
+
+int main(){
+    int a[10] = {7,2,3,6,5,10,4,9,8,1};
+    InsertSort(a, 10);
+    for(int i = 0; i < 10; i++)
+        cout << a[i] << " ";
+    cout << endl;
+}
