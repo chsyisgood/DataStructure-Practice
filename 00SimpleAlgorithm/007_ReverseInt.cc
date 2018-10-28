6a883be87e6198dcf60fa082ec2e450ec8b4bdba
chsyisgood <chsy973414741@gmail.com>
2018-10-28 17:32:39 -0400

    Update 00SimpleAlgorithm/007_ReverseInt.cc

--- 00SimpleAlgorithm/007_ReverseInt.cc
+++ 00SimpleAlgorithm/007_ReverseInt.cc
@@ -18,12 +18,11 @@ class Solution {
 public:
     int reverse(int x) {
         bool a = (x > 0);
-        long x_long = x;
-        if(x > INT32_MAX || x < INT32_MIN)
-            return 0;
-        if(!a) x_long = -x;
+        long x_long = (a)? x: -x;
         long ans = 0;
         while( x_long > 0){
+            if(ans > INT32_MAX/10 || ans < INT32_MIN/10)
+                return 0;
             ans =  ans * 10 + x_long % 10;
             x_long = x_long /10;
         }
