648b572f10e4127fc4c43886754b5e6aac270d9e
chsyisgood <chsy973414741@gmail.com>
2018-10-30 20:30:28 -0400

    Update 00SimpleAlgorithm/001_TwoSum.cc

--- 00SimpleAlgorithm/001_TwoSum.cc
+++ 00SimpleAlgorithm/001_TwoSum.cc
@@ -11,9 +11,6 @@ using namespace std;
   3. 这里的 Map.count(...) > 0 也可以使用 Map.find(...) != Map.end(), 两者效果类似，后者应该会好一点
   4. Map.at(...) = Map[...] , at会做下标检查，没有则报错；后者没有返回空值
 
-  https://onedrive.live.com/edit.aspx/%e6%96%87%e6%a1%a3/graduate%20study?cid=0e4a80fd2212bc8e&id=documents&wd=target%28C%5EM%5EM.one%7C35FE0337-74BD-471E-A5EB-2B9445917EB5%2FMap%2C%20hash%20map%2C%20unordered%20map%7C68862A80-EE78-46CD-B0A6-257F31CF98BF%2F%29
-onenote:https://d.docs.live.net/0e4a80fd2212bc8e/文档/graduate%20study/C%5eM%5eM.one#Map,%20hash%20map,%20unordered%20map&section-id={35FE0337-74BD-471E-A5EB-2B9445917EB5}&page-id={68862A80-EE78-46CD-B0A6-257F31CF98BF}&end
-
   算法：
   1. 逻辑：显然需要在原有的map中先查找 remnant， 然后再插入新的值 
   2. brute force 很容易写，是一个两层嵌套循环，复杂度很高
