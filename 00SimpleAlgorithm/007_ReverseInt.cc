39518993445eff78390f85135b77669299dc6ea3
chsyisgood <chsy973414741@gmail.com>
2018-10-31 03:03:46 -0400

    Update 00SimpleAlgorithm/007_ReverseInt.cc

--- 00SimpleAlgorithm/007_ReverseInt.cc
+++ 00SimpleAlgorithm/007_ReverseInt.cc
@@ -1,38 +1,41 @@
 #include <iostream>
-
 using namespace std;
+
 /*
-    0 ms 100.0%
-    LC 50 power(x, n) x is double from -100 to 100
-    n is integer from INT32_MIN to INT32_MAX
-    重点：
-        【界定】边界条件，通过所有的test cases !!!
-        防止【overflow】，需要其他类型的【int -> long】
-        学习如何自主【选择】 test cases
-        数据类型会影响速度，例如用long时为0 ms，用long long 为 4ms
-    难度：简单
+  学习：
+  1. 如何理解 overflow 和 题目中的条件，如何防止result不受到数据类型的影响？ Onenote\\C++\\Overflow
+  2. dealing with an environment which could only store integers within the 32-bit signed integer range INT32_MIN, INT32_MAX
+     这里的意思明显是说运算过程中的【中间结果 超过范围】则return 0
+  3. 负数的 % 和 / 运算
+
+  算法：
+  1. 重点：无需判断正负，算法都可以完成整数的反转：理解负数的运算！
 */
 
-int x = [](){ std::ios_base::sync_with_stdio(false); std::cin.tie(nullptr); return 0; }();
+static const auto speedup =[]() {std::ios::sync_with_stdio(false); std::cin.tie(NULL); return 0;}();
 class Solution {
 public:
     int reverse(int x) {
-        bool a = (x > 0);
-        long x_long = (a)? x: -x;
-        long ans = 0;
-        while( x_long > 0){
-            if(ans > INT32_MAX/10 || ans < INT32_MIN/10)
-                return 0;
+        long long x_long = x;
+        long long ans = 0;
+        while(x_long != 0){
             ans =  ans * 10 + x_long % 10;
+            if(ans > INT32_MAX || ans < INT32_MIN)
+                return 0;
             x_long = x_long /10;
         }
-        return (a)? ans: -ans;
+        return ans;
     }
 };
 
 int main(){
     Solution a;
+    cout << "INT32_MAX: " << INT32_MAX << endl;
+    cout << "INT32_MIN: " << INT32_MIN << endl;
     cout << a.reverse(145633) << '\n';
-    cout << a.reverse(INT32_MAX) << '\n';
-    cout << a.reverse(INT32_MIN) << '\n';
+    cout << a.reverse(-145633) << '\n';
+    cout << long(a.reverse(INT32_MAX)) << '\n';
+    cout << long(a.reverse(INT32_MIN)) << '\n';
+    cout << long(a.reverse(INT32_MAX - 7)) << '\n';
+    cout << long(a.reverse(INT32_MIN + 8)) << '\n';
 }
