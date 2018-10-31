dfbcf35078d4531ec8e57524cb77bdb787600aa4
chsyisgood <chsy973414741@gmail.com>
2018-10-30 20:21:59 -0400

    Update 00SimpleAlgorithm/001_TwoSum.cc

--- 00SimpleAlgorithm/001_TwoSum.cc
+++ 00SimpleAlgorithm/001_TwoSum.cc
@@ -4,6 +4,21 @@
 #include <unordered_map>
 using namespace std;
 
+/*
+  学习：
+  1. 学习了C++中map,hashmap,unordered map的简单使用， include# <map><hashp_map><unordered_map>
+  2. 使用map的简单遍历和查找，复杂度更低 O(1)
+  3. 这里的 Map.count(...) > 0 也可以使用 Map.find(...) != Map.end(), 两者效果类似，后者应该会好一点
+  4. Map.at(...) = Map[...] , at会做下标检查，没有则报错；后者没有返回空值
+
+  算法：
+  1. 逻辑：显然需要在原有的map中先查找 remnant， 然后再插入新的值 
+  2. brute force 很容易写，是一个两层嵌套循环，复杂度很高
+
+  Q：
+  1. C++ 的auto怎么用？ 如何标准地遍历一遍 map？
+*/
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -16,3 +31,11 @@ public:
         throw ("Not found exception");
     }
 };
+
+int main(){
+    Solution a;
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int target = 12;
+    vector<int> ans = a.twoSum(nums, target);
+    cout << ans[0] << ' ' << ans[1] << endl;
+}
