be2b5ef0019843fbaebb7b47d4614193d3a6d5b4
chsyisgood <chsy973414741@gmail.com>
2018-11-01 00:31:10 -0400

    Update 01String/020_ValidParentheses.cc

--- 01String/020_ValidParentheses.cc
+++ 01String/020_ValidParentheses.cc
@@ -2,31 +2,41 @@
 #include <cstring>
 using namespace std;
 
+/*
+  学习：
+  1. 堆栈的简单思路
+  2. 如何使用str中的各类方法？ Onenote\\C++\\string str.push_back(x), str.pop_back(), str.empty()?, str.back()
+
+  算法：简单
+  1. 搞清楚，这就是个简单的堆栈，如果是前，则入栈；不是(后)，则判断是否相等并出栈，当最后栈为空时则为 true
+
+  重点：
+
+*/
+
 static const auto speedup =[]() {std::ios::sync_with_stdio(false); std::cin.tie(NULL); return 0;}();
 class Solution {
 public:
     bool isValid(string s) {
         if(s.empty()) return true;
-    string stack = "0"; int count = 0; int len = s.length();
+    string stack; int len = s.length();
     for(int i = 0; i < len; i++){
         if(s[i] == '(' || s[i] == '{' || s[i] == '['){
             switch(s[i]){
-                case '(': {stack += ')'; ++count; break; }
-                case '{': {stack += '}'; ++count; break; }
-                case '[': {stack += ']'; ++count; break; }
+                case '(': {stack.push_back(')'); break;}
+                case '{': {stack.push_back('}'); break;}
+                case '[': {stack.push_back(']'); break;}
             }
         }
         else{
-            if(count > 0 && s[i] == stack[count]){
-                --count;
+            if(!stack.empty() && s[i] == stack.back()){
                 stack.pop_back();
-                if(count == 0) stack = "0";
             }
             else
                 return false;
         }
     }
-    return (count == 0)? true: false;
+    return (stack.empty())? true: false;
     }
 };
 
