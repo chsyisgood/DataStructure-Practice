fc558ce00cafddac310b270e6eb2e20781baf8c0
chsyisgood <chsy973414741@gmail.com>
2018-11-01 00:40:55 -0400

    Update 00SimpleAlgorithm/009_PalindromeNumber.cc

--- 00SimpleAlgorithm/009_PalindromeNumber.cc
+++ 00SimpleAlgorithm/009_PalindromeNumber.cc
@@ -5,12 +5,16 @@ using namespace std;
     0 ms 100.0%
     LC 50 power(x, n) x is double from -100 to 100
     n is integer from INT32_MIN to INT32_MAX
+
+    学习：
+    1. 【界定】边界条件，通过所有的test cases !!!
+    2. 防止【overflow】，需要其他类型的【int -> long】，学习如何自主【选择】 test cases
+
+    算法：简单
+    1. 算法就是改编自power(x,n)，复杂度是 O(logN)
+
     重点：
-        【界定】边界条件，通过所有的test cases !!!
-        防止【overflow】，需要其他类型的【int -> long】
-        学习如何自主【选择】 test cases
-        数据类型会影响速度，例如用long时为0 ms，用long long 为 4ms
-    难度：简单
+    1. 先判断 n 是否超出边界，是则直接return；当n为负数时，做出相应的变化 -> 变成正数的情况
 */
 
 int x = [](){ std::ios_base::sync_with_stdio(false); std::cin.tie(nullptr); return 0; }();
