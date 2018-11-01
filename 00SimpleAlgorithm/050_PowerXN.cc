c0b8d5ef90a6a4b1f9e9eee7ae8ad4857fe7e6f0
chsyisgood <chsy973414741@gmail.com>
2018-11-01 00:40:52 -0400

    Update 00SimpleAlgorithm/050_PowerXN.cc

--- 00SimpleAlgorithm/050_PowerXN.cc
+++ 00SimpleAlgorithm/050_PowerXN.cc
@@ -5,15 +5,19 @@ using namespace std;
     0 ms 100.0%
     LC 50 power(x, n) x is double from -100 to 100
     n is integer from INT32_MIN to INT32_MAX
+
+    学习：
+    1. 【界定】边界条件，通过所有的test cases !!!
+    2. 防止【overflow】，需要其他类型的【int -> long】，学习如何自主【选择】 test cases
+
+    算法：简单
+    1. 算法就是改编自power(x,n)，复杂度是 O(logN)
+
     重点：
-        【界定】边界条件，通过所有的test cases !!!
-        防止【overflow】，需要其他类型的【int -> long】
-        学习如何自主【选择】 test cases
-        数据类型会影响速度，例如用long时为0 ms，用long long 为 4ms
-    难度：简单
+    1. 先判断 n 是否超出边界，是则直接return；当n为负数时，做出相应的变化 -> 变成正数的情况
 */
 
-int x = [](){ std::ios_base::sync_with_stdio(false); std::cin.tie(nullptr); return 0; }();
+static const auto speedup =[]() {std::ios::sync_with_stdio(false); std::cin.tie(NULL); return 0;}();
 class Solution {
 public:
     double myPow(double x, int n) {
@@ -28,8 +32,8 @@ public:
         while( n_long > 0){
             if(n_long % 2 ==1 )
                 prod *= x;
-            n_long = n_long/2;
-            x = x * x;
+            n_long /= 2;
+            x *= x;
         }
         return prod;
     }
